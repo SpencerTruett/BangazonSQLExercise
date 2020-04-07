0034ef8dc1c11dce61561ead644aaffaa22378a1
Spencer Truett <srtruett1993@gmail.com>
2020-04-07 13:29:48 -0500

    first commit

---
+++ BangazonSQLExercise.sql
@@ -0,0 +1,7 @@
+﻿--SELECT e.FirstName, e.LastName, CASE 
+--	  WHEN e.IsSupervisor = 0 THEN 'FALSE'
+--	  WHEN e.IsSupervisor = 1 THEN 'TRUE'
+--	  END AS IsSupervisor, d.[Name] FROM Employee e
+--LEFT JOIN Department d ON d.Id = e.DepartmentId
+--ORDER BY d.[Name], e.LastName, e.FirstName;
+
