7d06d2975839c6503d1b6d96fdb4b8c9d81a311a
Spencer Truett <srtruett1993@gmail.com>
2020-04-07 21:40:03 -0500

    skipped 14; finished through 21

--- BangazonSQLExercise.sql
+++ BangazonSQLExercise.sql
@@ -64,8 +64,52 @@
 --GROUP BY tp.[Name]
 --ORDER BY NumberOfAttendees DESC
 
+--THIS ONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 --STILL WORKING ON THIS ONE
-SELECT e.FirstName + ' ' + e.LastName AS EmployeesWithoutComputers FROM Employee e
-LEFT JOIN ComputerEmployee ce ON ce.EmployeeId = e.Id
-LEFT JOIN Computer c ON c.Id = ce.ComputerId
-WHERE ce.UnassignDate IS NOT NULL
+--SELECT e.FirstName + ' ' + e.LastName AS EmployeesWithoutComputers FROM Employee e
+--LEFT JOIN ComputerEmployee ce ON ce.EmployeeId = e.Id
+--LEFT JOIN Computer c ON c.Id = ce.ComputerId
+--WHERE ce.UnassignDate IS NULL
+
+--SELECT  e.FirstName + ' ' + e.LastName AS FullName,
+-- CASE 
+--	  WHEN c.Make + ' ' + c.Manufacturer  IS NOT NULL THEN c.Make + ' ' + c.Manufacturer
+--	  WHEN c.Make + ' ' + c.Manufacturer IS NULL THEN 'N/A'
+--	  END AS ComputerInfo
+--FROM Employee e
+--LEFT JOIN ComputerEmployee ce ON ce.EmployeeId = e.Id
+--LEFT JOIN Computer c ON c.Id = ce.ComputerId
+
+--SELECT c.Make + ' ' + c.Manufacturer AS ComputerName, c.PurchaseDate FROM Computer c
+--WHERE c.PurchaseDate <= '2019-07-01'
+--AND c.DecomissionDate IS NULL
+
+--SELECT e.FirstName + ' ' + e.LastName AS FullName, COUNT(ce.EmployeeId) AS NumberOfComputers FROM Employee e
+--LEFT JOIN ComputerEmployee ce ON ce.EmployeeId = e.Id
+--GROUP BY e.FirstName, e.LastName
+
+--SELECT DISTINCT pt.[Name], COUNT(pt.[Name]) AS NumberOfUsers FROM PaymentType pt
+--GROUP BY pt.[Name]
+
+--SELECT TOP 10 p.Title, p.Price, c.FirstName + ' ' + c.LastName AS FullName FROM Product p
+--LEFT JOIN Customer c ON c.Id = p.CustomerId
+--ORDER BY p.Price DESC
+
+--SELECT TOP 10 p.Title, COUNT(op.ProductId) AS NumberOfOrders FROM Product p
+--LEFT JOIN OrderProduct op ON op.ProductId = p.Id
+--GROUP BY p.Title
+--ORDER BY NumberOfOrders DESC
+
+--SELECT c.FirstName + ' ' + c.LastName AS FullName, COUNT(o.CustomerId) AS NumberOfOrders FROM Customer c
+--LEFT JOIN [Order] o ON o.CustomerId = c.Id
+--GROUP BY c.FirstName, c.LastName
+--ORDER BY NumberOfOrders DESC
+
+
+-- WORKING ON THIS ONE AS WELL
+SELECT DISTINCT prt.[Name] AS ProductType, SUM(COUNT(op.ProductId) * p.Price) AS TotalSales FROM Product p
+LEFT JOIN ProductType prt ON prt.Id = p.ProductTypeId
+LEFT JOIN OrderProduct op ON op.ProductId = p.Id
+GROUP BY prt.[Name]
+ORDER BY TotalSales DESC
+
